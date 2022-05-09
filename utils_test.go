7649421c2afbc24af3a8e39219378ab773ea2475
Ciaran <cslemon96@gmail.com>
2022-05-10 00:26:00 +0200

    (testing) add passing makeHash test

--- utils_test.go
+++ utils_test.go
@@ -5,7 +5,8 @@ import (
 )
 
 func TestMakeHash(t *testing.T) {
-	if got, want := makeHash("testUpload", 2500), "4401a81d8f8905d0f82461fd46637e2f984e10b4"; got != want {
+	if got, want := makeHash("testUpload", 7), hash; got != want {
 		t.Errorf("makeHash() not functioning as expected.")
+		compareTest(got, want)
 	}
 }
